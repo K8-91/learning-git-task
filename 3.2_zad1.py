4f5976e46fca85753d64d92d71995944e998830e
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-06-19 00:28:36 +0200

    Count products

--- 3.2_zad1.py
+++ 3.2_zad1.py
@@ -10,3 +10,8 @@ for key, value in lista_zakupow.items():
     for element in value:
         capitalize_value.append(element.capitalize())
     print(capitalize_value)
+
+products_q=[]
+for key in lista_zakupow:
+    products_q.append(len(lista_zakupow[key]))
+print("W sumie kupuje", sum(quantity for quantity in products_q), "produktow")
