d68aa7b2e02fc11073ba53518be2bc6cdaae5f64
Kasia Raczkier <katarzyna.raczkier@gmail.com>
2020-06-19 00:24:57 +0200

    Iterate dictionary and display string

--- 3.2_zad1.py
+++ 3.2_zad1.py
@@ -2,3 +2,11 @@ lista_zakupow={
     "piekarnia" : ["chleb", "bulki", "paczek"],
     "warzywniak": ["marchew", "seler", "rukola"]
 }
+
+print("Idę do sklepu")
+for key, value in lista_zakupow.items():
+    print("Idę do", key.capitalize(), "i kupuje tam", end =" ")
+    capitalize_value=[]
+    for element in value:
+        capitalize_value.append(element.capitalize())
+    print(capitalize_value)
